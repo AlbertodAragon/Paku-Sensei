0752ecbf2aa1bdd828aea2609fab92cc3cb403ef
Alberto Martinez de Aragon <alberto.aragon13@gmail.com>
2024-04-03 18:04:18 +0100

    refactor-enemy-follows-hero

---
+++ mygame/src/maps/maps.js
@@ -0,0 +1,24 @@
+export const maps = [
+  [
+    "ycc)cc9ccw",
+    "a        b",
+    "a        b",
+    "a    (   b",
+    "%        b",
+    "a    (   b",
+    "a        b",
+    "a        b",
+    "xdd)dd)ddz",
+  ],
+  [
+    "yccccccccw",
+    "a        b",
+    ")        )",
+    "a        b",
+    "a        b",
+    "a    $   b",
+    ")   s    )",
+    "a        b",
+    "xddddddddz",
+  ],
+];
