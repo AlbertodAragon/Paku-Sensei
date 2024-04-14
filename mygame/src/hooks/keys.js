bd249bd9da83d24b3b65d619d30712b4e5775fdd
Alberto Martinez de Aragon <alberto.aragon13@gmail.com>
2024-04-14 15:48:55 +0100

    fix-animation-flip

--- mygame/src/hooks/keys.js
+++ mygame/src/hooks/keys.js
@@ -9,7 +9,7 @@ export const movement = (player) => {
     if (player.curAnim() !== "walk") {
       player.play("walk");
     }
-
+    player.flipX = false;
     // console.log("animation", animation);
     player.move(-HERO_SPEED, 0);
     // player.flipX(false);
@@ -21,17 +21,24 @@ export const movement = (player) => {
       player.play("walk");
     }
     player.move(HERO_SPEED, 0);
-    // player.flipX(true);
+    player.flipX = true;
     player.dir = vec2(1, 0);
   });
 
   onKeyDown("up", () => {
     // player.use(sprite("link-going-up"));
+    if (player.curAnim() !== "walk") {
+      player.play("walk");
+    }
     player.move(0, -HERO_SPEED);
+    console.log(player);
     player.dir = vec2(0, -1);
   });
 
   onKeyDown("down", () => {
+    if (player.curAnim() !== "walk") {
+      player.play("walk");
+    }
     // player.use(sprite("link-going-down"));
     player.move(0, HERO_SPEED);
     player.dir = vec2(0, 1);
